4210bef9a16ffe3ab0be4f56d26cfa13fdda406e
Vladislav Vashchenko <vlad.uknown@mail.ru>
2022-12-11 01:10:22 +0600

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { useState, useRef, useEffect } from "react";
+import logo from "../../resources/images/logo.svg";
+import human from "../../resources/images/human.svg";
+import search from "../../resources/images/search.svg";
+import smallCheck from "../../resources/images/smallCheck.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setMenuIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  }, [menuIsOpen]);
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img src={logo} alt="logo" draggable="false" />
+      <ul className='navbar__menu' ref={menuRef}>
+        <li>Пропало животное</li>
+        <li>Вет. клиники</li>
+
+        <li onClick={() => setMenuIsOpen(!menuIsOpen)}>Уход</li>
+        <img onClick={() => setMenuIsOpen(!menuIsOpen)} className={menuIsOpen ? 'navbar__menu__check--active' : 'navbar__menu__check--disabled'} src={smallCheck} alt="" draggable="false" />
+
+        <div className='navbar__menu__dropdown'>
+          <ul className={menuIsOpen ? "navbar__menu__dropdown--active" : "navbar__menu__dropdown--disabled"} >
+            <p onClick={() => {
+              setMenuIsOpen(!menuIsOpen);
+            }}>Кот</p>
+            <p onClick={() => {
+              setMenuIsOpen(!menuIsOpen);
+            }}>Собака</p>
+          </ul>
+        </div>
+        <li>Первая помощь</li>
+        <li>Приюты</li>
+        <img className="navbar__menu__icon" src={human} alt="" draggable="false" />
+        <img className="navbar__menu__icon" src={search} alt="" draggable="false" />
+      </ul>
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Head</h1>
     </div>
   )
 }
