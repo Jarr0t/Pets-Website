84d376d9a8d64223952db9a8b96424b700dd4d9d
Vladislav Vashchenko <vlad.uknown@mail.ru>
2022-12-10 22:02:47 +0600

    a file structure has been created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import MissingAnimal from "./pages/MissingAnimal.js";
+import VeterinaryClinics from "./pages/VeterinaryClinics.js";
+import Navbar from './components/Navbar/Navbar.js';
 
 const App = () => {
   return (
-    <div>
-      Hi
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/veterinaryclinics' element={<VeterinaryClinics />} />
+        <Route path='/missinganimals' element={<MissingAnimal />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/VeterinaryClinics.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VeterinaryClinics = () => {
+  return (
+    <div>Veterinary Clinics</div>
+  )
+}
+
+export default VeterinaryClinics;
